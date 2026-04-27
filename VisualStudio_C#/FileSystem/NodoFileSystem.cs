1d1ac8d90aeb7cef1d314c987e474943c6af9996
Valentina Naldi <valentina.naldi2011@gmail.com>
2026-04-27 17:21:30 +0200

    Add files via upload
    Creazione di un sistema ad albero per l'organizzazione file, la ricerca file/cartelle, la stampa del percorso e di report su dimensione cartelle e numero di file/cartelle

---
+++ VisualStudio_C#/FileSystem/NodoFileSystem.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    public class NodoFileSystem
+    {
+        //Proprietà di un Nodo del FileSystem
+        public string Nome { get; set; }
+        public int Dimensione { get; set; }
+        public List<NodoFileSystem> ListaFigli { get; set; } 
+        public NodoFileSystem NodoGenitore { get; set; }
+
+        private bool isCartella { get; set; } = false;
+
+
+        //Costruttore
+        public NodoFileSystem(string nome, int dimensione=0)
+        {
+            Nome = nome;
+            Dimensione = Math.Max(dimensione, 0);  //Se è 0 è una cartella
+            ListaFigli = new List<NodoFileSystem>();
+            if (dimensione == 0) isCartella = true;
+        }
+
+
+        //METODI
+        //Metodo che ritorna se cartella oppure file
+        public bool IdDir()
+        {
+            return isCartella;
+        }
+        //Metodo per creare un nodo
+        public void AggiungiNodo(NodoFileSystem nodo)
+        {
+            nodo.NodoGenitore = this;
+            ListaFigli.Add(nodo);
+        }
+
+        //Metodo per aggiungere un file al nodo chiamante
+        public void AggiungiFile(string nome, int dimensione)
+        {
+            var file= new NodoFileSystem(nome, dimensione);
+            AggiungiNodo(file);
+        }
+        //Metodo per aggiungere una cartella al nodo chiamante
+        public NodoFileSystem AggiungiCartella(string nome)
+        {
+            var cartella = new NodoFileSystem(nome);
+            AggiungiNodo(cartella);
+            return cartella;
+        }
+                
+
+        //Metodo per cercare un componente per nome  e restituire l'oggetto
+        public NodoFileSystem Trova(string nome)
+        {            
+            if (this.Nome == nome) return this;
+
+            foreach (NodoFileSystem figlio in this.ListaFigli)
+            {
+                NodoFileSystem risultato = figlio.Trova(nome);
+
+                if (risultato != null) return risultato;
+            }
+
+            return null;
+            
+        }
+
+        //Metodo per calcolare la dimensione totale di tutti i file di una cartella
+        public static int Somma(NodoFileSystem componente)
+        {
+            if (!componente.isCartella ) return componente.Dimensione; //se è un file ne ritorna la dimensiome
+
+            int dimTot = 0;
+
+            foreach (NodoFileSystem figlio in componente.ListaFigli)
+            {
+                dimTot += Somma(figlio);
+            }
+
+            return dimTot;
+        }
+
+        //Metodo per contare i file in una cartella
+        public static int ContaFile(NodoFileSystem cartella)
+        {
+            if (cartella == null) return 0;
+            int count = 0;
+            
+            foreach (NodoFileSystem n in cartella.ListaFigli)
+            {
+                if (!n.isCartella)
+                {
+                    count++;
+                }
+                else
+                {
+                    count += ContaFile(n); // ricorsione sulle sottocartelle
+                }
+            }
+
+            return count;
+        }
+
+        //Metodo per contare le cartelle in una cartella
+        public static int ContaCartelle(NodoFileSystem cartella)
+        {
+            if (cartella == null) return 0;
+            int count = 1;
+            
+            foreach (NodoFileSystem n in cartella.ListaFigli)
+            {
+                if (n.isCartella)
+                {
+                    //count++;
+                    count += ContaCartelle(n);
+                }
+                
+            }
+
+            return count;
+        }
+        
+        //Metodo per stampare il contenuto di una cartella
+        public void Stampa(int livello = 0)
+        {
+            
+            string indent = new string(' ', livello * 2);
+
+            Console.WriteLine($"{indent}{this.Nome}/");
+
+            foreach (NodoFileSystem n in this.ListaFigli)
+            {
+                if (n.isCartella)
+                    n.Stampa(livello + 1);
+                else
+                    Console.WriteLine($"{indent}  {n.Nome} ({n.Dimensione} KB)");
+            }
+        }
+        //Metodo per stampare il percorso di un elemento
+        public static string CostruisciPercorso(NodoFileSystem nodo)
+        {
+            var percorso=new List<string>();
+            var attuale = nodo;
+            while (attuale != null)
+            {
+                percorso.Add(attuale.Nome);
+                attuale = attuale.NodoGenitore;
+            }
+            percorso.Reverse(); //inverte gli elementi della lista
+            return string.Join("/", percorso); //ritorna la stringa con gli elementi concatenati con /
+        }
+    }
+}
---
+++ VisualStudio_C#/FileSystem/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definizione e Inizializzazione di cartelle e file
+            NodoFileSystem Radice = new NodoFileSystem("C:",0);
+            
+
+            //Aggiunta di file e cartelle ai nodi genitore            
+            var Users= Radice.AggiungiCartella("Users");
+            var System= Radice.AggiungiCartella("System");
+            Users.AggiungiFile("documento.txt", 50);
+            var Desktop= Users.AggiungiCartella("Desktop");
+            Desktop.AggiungiFile("foto.jpg", 2000);
+            var Documenti= Users.AggiungiCartella("Documenti");
+            Documenti.AggiungiFile("relazione.docx",500);
+            System.AggiungiFile("kernel.sys", 1000);
+            System.AggiungiFile("config.ini", 50);
+
+
+            //Calcolo Dimensione totale del disco
+            Console.WriteLine("==============REPORT==============");
+            Console.WriteLine($"Dimensione totale del disco= {NodoFileSystem.Somma(Radice)}");
+            Console.WriteLine($"Numero totale di file= {NodoFileSystem.ContaFile(Radice)}");
+            Console.WriteLine($"Numero totale di cartelle= {NodoFileSystem.ContaCartelle(Radice)}");
+            Console.WriteLine($"Dimensione di Users= {NodoFileSystem.Somma(Users)}");
+            Console.WriteLine("----------------------------------");
+
+            //Stampa del contenuto del DISCO
+            Console.WriteLine("Stampa del contenuto del DISCO");
+            Radice.Stampa();
+            Console.WriteLine("----------------------------------");
+
+            //Stampa del contenuto di Users
+            Console.WriteLine("Stampa del contenuto di 'Users'");
+            Users.Stampa();
+            Console.WriteLine("----------------------------------");
+
+            //Stampa del percorso di una cartella
+            string percorso_cartella_da_stampare = "Documenti";
+            NodoFileSystem ricercaCartella = Radice.Trova(percorso_cartella_da_stampare); //Ricerca una cartella per nome e restituisce il nodo
+            Console.WriteLine("Stampa del percorso della cartella 'Documenti'");
+            Console.WriteLine($"{NodoFileSystem.CostruisciPercorso(ricercaCartella)}");
+            Console.WriteLine();
+
+            //Ricerca della cartella di un file per Nome           
+            string File_da_cercare = "foto.jpg";
+            Console.WriteLine($"Ricerca file: {File_da_cercare}");
+            NodoFileSystem ricercaFile = Radice.Trova(File_da_cercare); //Ricerca un file per nome e restituisce il nodo
+            if (ricercaFile != null)
+            {
+                Console.WriteLine($"File trovato nella cartella: {ricercaFile.NodoGenitore.Nome}/");
+                Console.WriteLine($"Percorso completo: {NodoFileSystem.CostruisciPercorso(ricercaFile.NodoGenitore)}/{File_da_cercare}");               
+            }
+            else
+                Console.WriteLine($"File non trovato");
+            
+        }
+    }
+}
